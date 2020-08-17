34f6a558d59b2c406c3145d61f78ff6cc2d1e9cb
Payton Gray <paytongray18@gmail.com>
2020-08-17 18:24:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word(string)
   array = string.split("")
   new_array = []
-    array.each{|array| array.reverse}
-    new_array << array.join("")
-  end
+  array.each{|array| array.reverse}
+  new_array << array.join("")
 end
 
 
