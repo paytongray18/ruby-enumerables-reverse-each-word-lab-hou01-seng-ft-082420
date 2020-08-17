e72531768ecafff57879e3a741c67719f6754f4a
Payton Gray <paytongray18@gmail.com>
2020-08-17 19:08:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array == []
+  new_array = []
   new_array<< array.each{|element| element.reverse!}.join(" ")
 end
 
